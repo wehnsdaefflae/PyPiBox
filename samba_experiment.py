9f6f257a33478e81ae6b3e8f4330fde888a86c2b
Mark <wernsdorfer@gmail.com>
2023-04-09 00:23:16 +0200

    samba frenzy

--- samba_experiment.py
+++ samba_experiment.py
@@ -1,4 +1,7 @@
 from smb.SMBConnection import SMBConnection
+import smbclient
+from smbprotocol import Dialects
+from smbprotocol.connection import Connection
 
 
 def connect_to_samba_share():
@@ -6,19 +9,58 @@ def connect_to_samba_share():
     conn.connect("192.168.10.1")
     if conn:
         print("Connected to Samba share")
-        result = []
-        for file in conn.listPath("FRITZ.NAS", "/500GB/"):
-            print(file.filename, file.create_time, file.file_size)
+        for file in conn.listPath("FRITZ.NAS", "/500GB/", pattern="*"):
 
             if file.isDirectory:
-                result.append((file.filename + "/", file.create_time, file.file_size))
+                print(file.filename + "/", file.create_time, file.file_size)
             else:
-                result.append((file.filename, file.create_time, file.file_size))
-        return result
+                print(file.filename, file.create_time, file.file_size)
+
     else:
         print("Failed to connect to Samba share")
 
     conn.close()
 
-# print(netbios.queryIPForName("192.168.10.1"))
-connect_to_samba_share()
+
+def list_files_recursive(path, depth=0):
+    for item in smbclient.listdir(path):
+        item_path = f"{path}\\{item}"
+        if item.is_dir(item_path):
+            print("  " * depth + f"[D] {item}")
+            list_files_recursive(item_path, depth + 1)
+        else:
+            print("  " * depth + f"[F] {item}")
+
+
+def connect_to_samba_share():
+    connection = Connection(1212, "192.168.10.1", port=139)
+    connection.connect(Dialects.SMB_2_1_0)
+
+    if connection.is_connected():
+        session = connection.session_setup()
+        tree = session.tree_connect("FRITZ.NAS")
+        query_directory(tree, "/500GB/")
+        tree.disconnect()
+    else:
+        print("Failed to connect to Samba share")
+
+    connection.disconnect()
+
+
+def connect() -> None:
+    # c = smbclient.ClientConfig(username="mark", password="6bB3gv%2zeF4wxJ(")
+
+    # share_path = "\\\\192-168-10-1\\500GB"
+    share_path = "//192-168-10-1/500GB"
+
+    # list_files_recursive(share_path)
+
+    smbclient.register_session("192.168.10.1", username="mark", password="6bB3gv%2zeF4wxJ(", port=139)
+    for each_file in smbclient.walk("//192-168-10-1/FRITZ.NAS/500GB"):  # listDir, walk, scandir
+        print(each_file)
+
+
+
+if __name__ == "__main__":
+    # connect_to_samba_share()
+    connect()
