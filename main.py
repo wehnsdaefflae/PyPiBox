1918acfabd75ffac3f1da00170bf411f6f80422c
mark <wernsdorfer@gmail.com>
2023-04-08 23:50:57 +0200

    finally samba

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 from __future__ import annotations
 import json
-import os
 import sys
 import pathlib
 import time
---
+++ samba_experiment.py
@@ -0,0 +1,24 @@
+from smb.SMBConnection import SMBConnection
+
+
+def connect_to_samba_share():
+    conn = SMBConnection("mark", "6bB3gv%2zeF4wxJ(", "192.168.10.24", "192-168-10-1")
+    conn.connect("192.168.10.1")
+    if conn:
+        print("Connected to Samba share")
+        result = []
+        for file in conn.listPath("FRITZ.NAS", "/500GB/"):
+            print(file.filename, file.create_time, file.file_size)
+
+            if file.isDirectory:
+                result.append((file.filename + "/", file.create_time, file.file_size))
+            else:
+                result.append((file.filename, file.create_time, file.file_size))
+        return result
+    else:
+        print("Failed to connect to Samba share")
+
+    conn.close()
+
+# print(netbios.queryIPForName("192.168.10.1"))
+connect_to_samba_share()
